d192e736ba8c7faaeaef981b6656d9e6b0a0fa3d
Skaldak <Skaldak42@users.noreply.github.com>
2019-04-29 00:47:45 +0200

    Add search & autocomplete

--- Project-Map/WebContent/Metro.js
+++ Project-Map/WebContent/Metro.js
@@ -1,14 +1,3 @@
-function getCircle(magnitude) {
-    return {
-        path: google.maps.SymbolPath.CIRCLE,
-        fillColor: 'red',
-        fillOpacity: .2,
-        scale: (magnitude - 177017) / (50860744 - 177017) * 42,
-        strokeColor: 'white',
-        strokeWeight: .5
-    };
-}
-
 function initMap() {
     var parisCenter = {
         lat: 48.8566,
@@ -22,14 +11,76 @@ function initMap() {
         position: parisCenter,
         map: map
     });
-    
+
     initStation(map);
+    initSearch(map);
+
+    // var directionsService = new google.maps.DirectionsService();
+    // var directionsDisplay = new google.maps.DirectionsRenderer();
+    // var srcLatLng = new google.maps.LatLng(48.88430478953, 2.3670850145355);
+    // var dstLatLng = new google.maps.LatLng(48.93761835474619, 2.3620002027025153);
+
+    // directionsDisplay.setMap(map);
+    // getRoute(srcLatLng, dstLatLng, directionsService, directionsDisplay);
 }
 
-function initCorrespondence() {
-    $.getJSON("reseau.json", function (Metro) {
-        var Correspondence = Metro.corresp;
-    })
+function initSearch(map) {
+    var search = document.getElementById('search');
+    var input = document.getElementsByClassName('searchInput');
+
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(search);
+    initAutocomplete(map, input[0]);
+    initAutocomplete(map, input[1]);
+}
+
+function initAutocomplete(map, input) {
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.bindTo('bounds', map);
+    autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+
+    var info = new google.maps.InfoWindow();
+    var infoContent = document.getElementById('infoContent');
+
+    info.setContent(infoContent);
+
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    autocomplete.addListener('place_changed', function () {
+        info.close();
+        marker.setVisible(false);
+
+        var place = autocomplete.getPlace();
+
+        if (!place.geometry) {
+            window.alert("404 '" + place.name + "'");
+            return;
+        } else if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(14);
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+        infoContent.children['placeIcon'].src = place.icon;
+        infoContent.children['placeName'].textContent = place.name;
+        infoContent.children['placeAddress'].textContent = address;
+        info.open(map, marker);
+    });
 }
 
 function initStation(map) {
@@ -42,23 +93,21 @@ function initStation(map) {
                 map: map,
                 position: latlng,
                 title: station,
-                // icon: getCircle(10)
                 icon: getCircle(getTraffic(stationInfomation.nom))
             });
         })
     })
 }
 
-function initRoute() {
-    $.getJSON("reseau.json", function (Metro) {
-        var Route = Metro.routes;
-    })
-}
-
-function initLine() {
-    $.getJSON("reseau.json", function (Metro) {
-        var Line = Metro.lignes;
-    })
+function getCircle(magnitude) {
+    return {
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: 'red',
+        fillOpacity: .2,
+        scale: (magnitude - 177017) / (50860744 - 177017) * 42,
+        strokeColor: 'white',
+        strokeWeight: .5
+    };
 }
 
 function getTraffic(station) {
@@ -83,4 +132,22 @@ function getTraffic(station) {
         return 0;
     else
         return traffic;
+}
+
+function getRoute(src, dst, directionsService, directionsDisplay) {
+    var directionsRequest = {
+        origin: src,
+        destination: dst,
+        travelMode: "TRANSIT",
+        transitOptions: {
+            modes: ['RAIL']
+        }
+    };
+
+    directionsService.route(directionsRequest, function (result, status) {
+        if (status == 'OK') {
+            alert(status);
+            directionsDisplay.setDirections(result);
+        }
+    })
 }
--- Project-Map/WebContent/map.jsp
+++ Project-Map/WebContent/map.jsp
@@ -7,39 +7,38 @@
 <html>
 
 <head>
-<meta charset="ISO-8859-1">
-<title>Safe Map</title>
-<script
-	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVIc1EU_Lxw-8Yh5Mt82rgtKCkbKb0Rbg&callback=initMap"
-	async defer></script>
-<script type="text/javascript"
-	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVIc1EU_Lxw-8Yh5Mt82rgtKCkbKb0Rbg&libraries=places"></script>
-<script src="jquery-3.3.1.js" type="text/javascript"></script>
-<script src="Metro.js" type="text/javascript"></script>
-<style>
-#map {
-	height: 100%;
-	width: 85%;
-}
-
-html, body {
-	height: 100%;
-	margin: 0;
-	padding: 0;
-}
-</style>
+	<meta charset="ISO-8859-1">
+	<title>MapX</title>
+	<link rel="stylesheet" type="text/css" href="theme.css">
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVIc1EU_Lxw-8Yh5Mt82rgtKCkbKb0Rbg&callback=initMap"
+		async defer>
+	</script>
+	<script type="text/javascript"
+		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVIc1EU_Lxw-8Yh5Mt82rgtKCkbKb0Rbg&libraries=places">
+	</script>
+	<script src="jquery-3.3.1.js" type="text/javascript"></script>
 </head>
 
 <body>
-	<div id="map"></div>
-	<div id="search">
-		<form id="searchForm" method="GET">
-			<fieldset>
-				<p>
-					<label></label>
-				</p>
-			</fieldset>
-		</form>
+	<div id="search" class="search">
+		<div id="searchContainer" class="searchContainer">
+			<p>
+				<input id="srcInput" class="searchInput" type="text" placeholder="Source">
+			</p>
+			<p>
+				<input id="dstInput" class="searchInput" type="text" placeholder="Destination">
+			<p>
+		</div>
 	</div>
+	<div id="map">
+		<div id="infoContent">
+			<img src="" width="16" height="16" id="placeIcon">
+			<span id="placeName" class="title"></span>
+			<br>
+			<span id="placeAddress"></span>
+		</div>
+	</div>
+	<script src="Metro.js" type="text/javascript"></script>
 </body>
+
 </html>
